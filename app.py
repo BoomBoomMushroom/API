9ce1fd304151d219756fc7066a7d8ff684b44fdb
BoomBoomMushroom <77977795+BoomBoomMushroom@users.noreply.github.com>
2021-08-07 15:27:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from os import abort
 import flask
 from flask import request
 from flask import json
@@ -22,3 +23,43 @@ def gcPublishLvl():
 @app.route("/gcGetLvls", methods=["GET"])
 def gcGetLvls():
     return json.dumps(GameshubApi.getGameCreatorLevels())
+@app.route("/checkUsername", methods=["GET"])
+def checkUsername():
+    try:
+        requestJson = request.json
+        if requestJson["Username"]:
+            return GameshubApi.checkUsername(requestJson["Username"])
+    except:
+        abort(400)
+@app.route("/signup", methods=["POST"])
+def checkUsername():
+    try:
+        requestJson = request.json
+        if requestJson["Username"] and requestJson["Password"]:
+            return GameshubApi.signup(requestJson["Username"],requestJson["Password"])
+    except:
+        abort(400)
+@app.route("/delacc",method=["POST"])
+def delacc():
+    try:
+        requestJson = request.json
+        if requestJson["Token"]:
+            return GameshubApi.deleteAccount(requestJson["Token"])
+    except:
+        abort(400)
+@app.route("/login",method=["GET"])
+def login():
+    try:
+        requestJson = request.json
+        if requestJson["Username"] and requestJson["Password"]:
+            return GameshubApi.login(requestJson["Username"],requestJson["Password"])
+    except:
+        abort(400)
+@app.route("/logout",method=["GET"])
+def logout():
+    try:
+        requestJson = request.json
+        if requestJson["Token"]:
+            return GameshubApi.logout(requestJson["Token"])
+    except:
+        abort(400)
