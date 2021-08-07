5e750b717517257010ec93ea67914fbd3089dc84
BoomBoomMushroom <77977795+BoomBoomMushroom@users.noreply.github.com>
2021-08-07 12:29:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 import flask
 from flask import request
+from flask import json
+
+import GameshubApi
 
 app = flask.Flask(__name__)
 
 
 @app.route("/")
 def home():
-    print(f"Hello {request.method} user!")
     return f"Hello {request.method} user!"
+
+@app.route("/gcPublishLvl", methods=["POST"])
+def gcPublishLvl():
+    try:
+        jsonSent = request.json
+        htmlData = jsonSent["HTML"]
+        gameInfoData = jsonSent["GameInfo"]
+    except:
+        jsonSent = {}
+    GameshubApi.gameCreatorPublishLevel(htmlData,gameInfoData)
+@app.route("/gcGetLvls", methods=["GET"])
+def gcGetLvls():
+    return GameshubApi.getGameCreatorLevels()
